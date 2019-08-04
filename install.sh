a3f8dc8b1e536f596aeffce50f8220ab19bf85cc
Johannes Filter <hi@jfilter.de>
2019-08-04 17:21:01 +0200

    add pathogen

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 ln -s ~/utils/dotfiles/.vimrc ~/.vimrc
 ln -s ~/utils/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/utils/dotfiles/.bash_aliases ~/.bash_aliases
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim &&
+cd ~/.vim/bundle && git clonse https://github.com/brendonrapp/smyck-vim
