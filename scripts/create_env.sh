e7a28c7a49349fde64e8ceec0973b6f9ff206150
Johannes Filter <hi@jfilter.de>
2021-06-28 00:00:06 +0200

    add option to specify python version for env

--- scripts/create_env.sh
+++ scripts/create_env.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 
-conda create -n $1
+# usage `cn someenv python=3.8` to specify the Python version
+
+conda create -n "$1" "$2"
 eval "$(conda shell.bash hook)"
-conda activate $1
+conda activate "$1"
 conda install -y -c conda-forge pandas poppler jupyterlab_code_formatter jupyterlab nodejs matplotlib black isort pylint scikit-learn spacy tqdm
 pip install pillow pdf2image
 jupyter labextension install @ryantam626/jupyterlab_code_formatter
